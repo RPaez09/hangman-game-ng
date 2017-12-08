8da9c24d298e2f4afe729b476e9a7e06c230bc70
Richard Paez <Richardp809@gmail.com>
2017-12-08 15:36:55 -0500

    Baseline layout created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { ViewEncapsulation } from '@angular/core/src/metadata/view';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Hangman Game';
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { Input } from '@angular/core/src/metadata/directives';
+
 if (environment.production) {
   enableProdMode();
 }
