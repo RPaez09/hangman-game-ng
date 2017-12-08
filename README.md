36a61f6c1ec524150ebbdbdcfe2831be67a4f473
Richard Paez <Richardp809@gmail.com>
2017-12-08 13:55:30 -0500

    View encapsulation removed from angular app

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # HangmanGame
 
+HangmanGame will not use view encapsulation in order to reduce number of files and since the project wont be too big.
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.0.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core/src/metadata/view';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hangman Game';
 }
